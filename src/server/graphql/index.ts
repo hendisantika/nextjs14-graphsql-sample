69fb4828698d30c508bc1a34ff12115f9021a9b4
hendisantika <hendisantika@yahoo.co.id>
2024-08-12 20:33:18 +0700

    Add schema.ts🫸🌀✏️📗🐧🐳

--- src/server/graphql/index.ts
+++ src/server/graphql/index.ts
@@ -1,6 +1,6 @@
 import {buildSchema} from "garph";
 
 import {resolvers} from "./resolvers";
-import {g} from "./src/app/schema";
+import {g} from "./schema";
 
 export const schema = buildSchema({g, resolvers});
